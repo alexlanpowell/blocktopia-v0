808a37318dfd1b0b4bbd78e760d9e9fce17fb8f8
Alexander Lan-Powell <turntopia@gmail.com>
2025-11-21 17:24:18 -0800

    v1.0.18: Add missing dependencies & bump version

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "Blocktopia",
     slug: "blocktopia",
     owner: "turntopia",
-    version: "1.0.17",
+    version: "1.0.18",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
